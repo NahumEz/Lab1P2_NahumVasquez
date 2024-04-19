72e4b6544b50ad7382c07399ab9d33149e139cf1
NahumEz <nahumvasquez0@gmail.com>
2024-04-19 13:24:28 -0600

    Primer Commit

---
+++ src/lab1p2_nahumvasquez/Lab1P2_NahumVasquez.java
@@ -0,0 +1,11 @@
+package lab1p2_nahumvasquez;
+import java.util.Scanner;
+public class Lab1P2_NahumVasquez {
+
+ 
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       
+    }
+    
+}
