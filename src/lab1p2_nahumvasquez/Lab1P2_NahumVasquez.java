321f09ede4e593ef3e2ce322b908590c3090ccc1
NahumEz <nahumvasquez0@gmail.com>
2024-04-19 14:02:15 -0600

    segundo commit

--- src/lab1p2_nahumvasquez/Lab1P2_NahumVasquez.java
+++ src/lab1p2_nahumvasquez/Lab1P2_NahumVasquez.java
@@ -1,11 +1,38 @@
 package lab1p2_nahumvasquez;
+
 import java.util.Scanner;
+import java.util.Random;
+
 public class Lab1P2_NahumVasquez {
 
- 
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       
+        Scanner sc = new Scanner(System.in);
+        int size;
+        System.out.println("Ingrese el tamanio de matriz deseado: ");
+        size = sc.nextInt();
+        crearMatriz(size);
+        imprimirMatriz(crearMatriz(size));
+        
     }
-    
+
+    public static int[][] crearMatriz(int x) {
+        Random rand = new Random();
+        int[][] matriz = new int[x][x];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = rand.nextInt(9);
+            }
+        }
+        return matriz;
+    }
+    public static int [][] imprimirMatriz(int [][] matrizgenerada){
+         for (int i = 0; i < matrizgenerada.length; i++ ){
+            for (int j = 0; j < matrizgenerada[i].length; j++){
+                System.out.print("["+matrizgenerada[i][j]+"]");
+            }
+            System.out.println("");
+        }
+         return matrizgenerada;
+    }
+
 }
